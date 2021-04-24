60d6db693b0c95c116efe7116e1d3b19194fc873
DanSoW <67583913+DanSoW@users.noreply.github.com>
2021-04-24 13:44:51 +0800

    added system upload and load files

--- SpringServer/server/src/main/java/com/server/controllers/FileUploadController.java
+++ SpringServer/server/src/main/java/com/server/controllers/FileUploadController.java
@@ -3,21 +3,28 @@ package com.server.controllers;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
-public class FileUploadController {
-	//класс-контроллер, для загрузки изображения на сервер
-	
-	private static final String nameDirectory = "Images"; //название директории, в которой будет находиться изображение
+import com.server.database.elements.DataElementImageFilePath;
 
-	@RequestMapping(value="/upload", method=RequestMethod.POST) //обработка POST-запроса загрузки изображения
+@RestController
+public class FileUploadController {
+	
+	private static final String nameDirectory = "Images";
+
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public @ResponseBody String handleFileUpload(
 			@RequestParam("name") String name,
 			@RequestParam("file") MultipartFile file
@@ -40,4 +47,30 @@ public class FileUploadController {
 		}
 		return "";
 	}
+	
+	@RequestMapping(value = "/load/{file_name:.+}", method = RequestMethod.GET)
+    public void getFile(@PathVariable("file_name") String fileName, HttpServletResponse response) {
+		File file = new File(nameDirectory + "\\" + fileName);
+        if (file.exists()){
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            response.setContentType("multipart/form-data");
+ 
+            try {
+                Files.copy(file.toPath(), response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException e) {
+                throw new RuntimeException("IOError writing file to output stream");
+            }
+        }
+    }
+	
+	@RequestMapping(value = "/load/filepath/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public DataElementImageFilePath getAbsoluteFilePath(@RequestParam String fileName) {
+		File file = new File(nameDirectory + "\\" + fileName);
+        if (file.exists()){
+            return new DataElementImageFilePath(file.getAbsolutePath());
+        }
+        
+        return new DataElementImageFilePath(null);
+    }
 }
